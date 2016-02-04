b3306b8e94b5a6fb1baea4c42098037d8c58ee8c
Carter Weiss <benanddana92@gmail.com>
2016-02-04 08:43:37 -0800

    made changes to pythonchallenge 3

--- CarterLevel4/src/pythonchallenge/challenge3v2Test.java
+++ CarterLevel4/src/pythonchallenge/challenge3v2Test.java
@@ -9,8 +9,12 @@ public class challenge3v2Test {
 	Challenge3v2 challenge3 = new Challenge3v2();
 	
 	@Test
-	public void test() {
+	public void testBodyguarded() {
 		assertTrue(challenge3.isBodyguarded("WDZjUZM"));
 	}
 
+	@Test
+	public void findSmall() {
+		assertEquals('d', challenge3.findSmallLetter("JDKSLDKdDKSIE"));
+	}
 }
