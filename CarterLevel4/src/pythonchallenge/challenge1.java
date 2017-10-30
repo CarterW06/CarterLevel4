0dc3c1c5b6b90389990fdf7313d34298d6f17447
Dana <Dana@Dana-PC.computer.Carter-PC.com>
2017-10-30 10:33:58 -0700

    Created new Challenge #3 program

--- CarterLevel4/src/pythonchallenge/challenge1.java
+++ CarterLevel4/src/pythonchallenge/challenge1.java
@@ -3,7 +3,7 @@ package pythonchallenge;
 
 public class challenge1 {
 	public static void main(String[] args) {
-		String s = "map";
+		String s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.";
 		String alphabet = "abcdefghijklmnopqrstuvwxyzab";
 		System.out.println(alphabet.length());
 		for (char c : s.toCharArray()) {
